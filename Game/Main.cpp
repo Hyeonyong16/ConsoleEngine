bb8801fa513dfadeb9726125f6ee04dea7bca3be
Hyeonyong16 <dizzyking77@naver.com>
2025-07-22 17:11:23 +0900

    Vector2 Class 추가
    - Vector2 클래스 추가
    - Vector2 내용 출력 함수 ToString 추가
    - +, - 연산자 오버로딩 추가

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include "Engine.h"
+#include "Math/Vector2.h"
+
+#ifdef _DEBUG
+#define new new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+// allocations to be of _CLIENT_BLOCK type
+#else
+#define new new
+#endif
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	//_CrtSetBreakAlloc(18);
+
+	Vector2 a(10, 20);
+	Vector2 b(20, 30);
+	std::cout << (a + b).ToString() << "\n";
+
 	Engine engine;
 	engine.Run();
 
